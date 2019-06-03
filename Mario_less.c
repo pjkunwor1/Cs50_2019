2d6518e09642c1318414333dfa85099578c1f284
Puspa Kunwor <pjkunwor1@gmail.com>
2019-06-03 21:26:42 +0000

    added comments

--- Mario_less.c
+++ Mario_less.c
@@ -5,14 +5,18 @@ int main(void)
 {
     int Height = 0;
     int k = 1;
+    //prompt user to put correct height of Mario Stairs 
 do {
     Height = get_int("Height:");
 } while ( Height < 1 || Height > 8);
-    
+
+//outer loop for the height of stairs 
 for (int m = Height ; m > 0; m--){
+        //loop for the blanks 
        for(int i = Height ; i > 1; i--) {
         printf(" ");
         }
+        //Actual loop for stairs 
         for(int j = 0; j < k; j++) {
         printf("#");
         }
